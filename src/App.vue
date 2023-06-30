7b01d23b23f7f6dacf3a33b60ad852b0ea0f1566
= <=>
2023-06-30 11:45:22 -0300

    roteiro de estudo

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  
+  <main>
+    <router-view/>
+  </main>
+  
+  <aside>
+    <nav class="menu">
+      <router-link to="/" class="home" >Home</router-link>
+      <router-link to="/about" class="about">About</router-link> 
+    </nav>
+  </aside>
+  <footer>FOOTER</footer>
+</template>
+
+<style scoped>
+.menu{ 
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
